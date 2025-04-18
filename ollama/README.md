4884c3bcc192ea30b509214f1d8bd5c811b38beb
shorelskyi <jhorels@jci.com>
2025-04-18 17:26:58 +0200

    Add ollama AI

---
+++ ollama/README.md
@@ -0,0 +1,9 @@
+# Run Vulkan/ROCm LLaMa
+
+docker compose build   
+
+docker compose up
+docker compose stop 
+docker compose rm
+
+
---
+++ ollama/start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export $(grep -v '^#' .env | xargs)
+
+
+echo "Launching OLLAMA AI..."
+echo "OLLAMA MODEL: ${OLLAMA_LLM_MODEL}"
+echo "OLLAMA DATA PATH: ${OLLAMA_DATA}"
+echo "Open-WEBUI DATA PATH: ${WEBUI_DATA}"
+
+if [[ -z ${OLLAMA_DATA} ]]; then
+    echo "OLLAMA data path ${OLLAMA_DATA} not found. Creating..."
+    mkdir -p ${OLLAMA_DATA}
+fi
+
+if [[ -z ${WEBUI_DATA} ]]; then
+    echo "Open-WEBUI data path ${WEBUI_DATA} not found. Creating..."
+    mkdir -p ${WEBUI_DATA}
+fi
+
+echo "Starting service on http://${HOST_IP}:${HOST_PORT}..."
+
+docker compose up
